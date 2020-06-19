ee5645385f1141721ccae3e17902a0340ec30b71
SoCuul <socuul@outlook.com>
2020-06-19 12:30:21 -0700

    Publish Code

---
+++ index.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+client.on('ready', () => {
+  console.log('Embed Bot is ready for action!');
+});
+
+
+//Bot Status
+client.on('ready', () => {
+client.user.setActivity(`${config.prefix}embedhelp`, { type: 'WATCHING' });
+});
+
+
+client.on('message', message => {
+//Help Command
+if (message.content.startsWith(config.prefix + "embedhelp")) {
+	const about = new Discord.MessageEmbed()
+	.setColor('#16ec50')
+	.setTitle('Embed Bot')
+	.setDescription('These are the current Embed Bot Commands!')
+	.setAuthor('Created by SoCuul', 'https://i.imgur.com/lKuBaAt.png', '')
+	.addFields(
+		{ name: 'Send embed', value: `${config.prefix}embed Title_Message`},
+		{ name: 'Restart Bot', value: `${config.prefix}restartembedbot`})
+		.setTimestamp()
+	message.channel.send(about);
+    }else
+//Embed Command
+if (message.content.startsWith(config.prefix + "embed")) {
+	if(message.member.roles.cache.has(config.staffid)) {
+		const command = config.prefix + "embed"
+		const args = message.content.slice(command.length).trim().split(/ +/g);
+		const announcement = args.slice(0).join(" ")
+		var parts = announcement.split('_', 2);
+		const announcementembed = new Discord.MessageEmbed()
+		.setColor(`#00B0ED`)
+		.addFields(
+			{ name: parts[0], value: parts[1]},)
+			.setTimestamp()
+			.setFooter(`Sent by ${message.author.username}`, message.author.avatarURL)
+			message.delete()
+			message.channel.send(announcementembed);
+			//Console log
+			console.log('----------------------------------------------------------------------------')
+			console.log(`${message.author.username} embedded the following message: `)
+			console.log(`Channel: ${message.channel}`)
+			console.log(`Title: ${parts[0]}`)
+			console.log(`Message: ${parts[1]}`);
+			console.log('----------------------------------------------------------------------------')
+			console.log('')   
+	}else {
+message.channel.send(`Sorry, but you don't have the correct permissions to use this command :(`)
+}}else
+//Restart Bot Process
+if (message.content.startsWith(config.prefix + "restartembedbot")) {
+	if(message.member.roles.cache.has(config.staffid)) {
+		console.log(`Embed Bot is restarting...`);
+		message.channel.send('Embed Bot is restarting...').then(sentMessage =>  process.exit(0))
+}else {
+message.channel.send(`Sorry, but you don't have the correct permissions to use this command :(`)
+}}});
+
+
+client.login(config.token);
