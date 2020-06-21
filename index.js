6b81e5ec539c81168edc9651cf39dc4f3e6876c0
SoCuul <socuul@outlook.com>
2020-06-21 14:17:58 -0700

    Added a Custom Colour command.

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ if (message.content.startsWith(config.prefix + "embedhelp")) {
 	.setAuthor('Created by SoCuul', 'https://i.imgur.com/lKuBaAt.png', '')
 	.addFields(
 		{ name: 'Send embed', value: `${config.prefix}embed Title_Message`},
+		{ name: 'Send custom colour embed (In hex or HTML Colour Names)', value: `${config.prefix}colourembed Colour_Title_Message`},
 		{ name: 'Send rainbow embed', value: `${config.prefix}rainbowembed Title_Message`},
 		{ name: 'Restart Bot', value: `${config.prefix}restartembedbot`})
 		.setTimestamp()
@@ -50,6 +51,32 @@ if (message.content.startsWith(config.prefix + "embed")) {
 			console.log(`Channel: ${message.channel}`)
 			console.log(`Title: ${parts[0]}`)
 			console.log(`Message: ${parts[1]}`);
+			console.log('----------------------------------------------------------------------------')
+			console.log('')   
+	}else {
+message.channel.send(`Sorry, but you don't have the correct permissions to use this command :(`)
+}}else
+//Custom Colour Embed Command
+if (message.content.startsWith(config.prefix + "colourembed")) {
+	if(message.member.roles.cache.has(config.staffid)) {
+		const command = config.prefix + "colourembed"
+		const args = message.content.slice(command.length).trim().split(/ +/g);
+		const announcement = args.slice(0).join(" ")
+		var parts = announcement.split('_', 2);
+		const announcementembed = new Discord.MessageEmbed()
+		.setColor(`${parts[0]}`)
+		.addFields(
+			{ name: parts[1], value: parts[2]},)
+			.setTimestamp()
+			.setFooter(`Sent by ${message.author.username}`, message.author.avatarURL)
+			message.delete()
+			message.channel.send(announcementembed);
+			//Console log
+			console.log('----------------------------------------------------------------------------')
+			console.log(`${message.author.username} embedded the following message: `)
+			console.log(`Channel: ${message.channel}`)
+			console.log(`Title: ${parts[1]}`)
+			console.log(`Message: ${parts[2]}`);
 			console.log('----------------------------------------------------------------------------')
 			console.log('')   
 	}else {
